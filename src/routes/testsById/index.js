07998337d0beddf9e99c834bafa33c1cb3b976a1
kandarp43 <kandarpdangi@gmail.com>
2024-02-14 23:39:20 +0530

    added loader

--- src/routes/testsById/index.js
+++ src/routes/testsById/index.js
@@ -3,15 +3,25 @@ import { useParams } from 'react-router-dom'
 import { useQuery } from 'react-query'
 import { testById } from '../../Query/tests/index.query'
 import QuestionsList from '../../components/test-module/questions-list'
+import { CircularProgress } from '@nextui-org/react'
 
 export default function TestsById() {
 	const { id } = useParams()
-	const { data } = useQuery(['getTestById', id], () => testById(id), {
-		enabled: !!id,
-		select: (d) => d.data,
-	})
+	const { data, isLoading } = useQuery(
+		['getTestById', id],
+		() => testById(id),
+		{
+			enabled: !!id,
+			select: (d) => d.data,
+		}
+	)
 	return (
 		<div className='my-4 px-4 sm:px-0 container mx-auto'>
+			{isLoading ? (
+				<div className='w-full flex items-center justify-center'>
+					<CircularProgress color='secondary' aria-label='Loading...' />
+				</div>
+			) : null}
 			<QuestionsList key={id} testData={data?.test} testId={id} />
 		</div>
 	)
